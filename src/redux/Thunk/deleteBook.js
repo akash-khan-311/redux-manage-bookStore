b6363939dd6cc011801c74c4effd4aa218f463f0
Akash Khan <akashcc48@gmail.com>
2024-02-12 14:14:42 +0600

    delete thunk added

---
+++ src/redux/Thunk/deleteBook.js
@@ -0,0 +1,17 @@
+import { removeBook } from "../BookStore/action";
+
+const deleteBook = (id) => {
+  return async (dispatch) => {
+    try {
+      await fetch(`http://localhost:9000/books/${id}`, {
+        method: "DELETE",
+      });
+
+      dispatch(removeBook(id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export default deleteBook;
